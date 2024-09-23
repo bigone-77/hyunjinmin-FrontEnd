a0b47fb8e2ebef5469ce28da51df114425931746
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-09-23 14:32:51 +0900

    🧪 Test: git 최신식으로 맞추기

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -2,7 +2,7 @@ import { Routes as ReactRouters, Route } from 'react-router-dom';
 
 import Navbar from '@shared/Navbar';
 
-import HomePage from '@/pages/home';
+import HomePage from '@/pages/Home';
 
 import LoginPage from '@pages/auth/Login';
 
