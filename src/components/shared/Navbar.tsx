00e8173428e25bb7a207f8b3e6966e0df894bd1a
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2024-09-23 14:24:28 +0900

    🧪 Test: github action 빌드 실패 확인

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,15 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { AnimatePresence, useCycle } from 'framer-motion';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 import { flexCenter, flexRowSpaceBetweenCenter } from '@styles/flex';
-import SideBar from '@components/sideBar';
 
 function Navbar() {
-  const [open, cycleOpen] = useCycle(false, true);
-
   const [navHeight, setNavHeight] = useState(0);
   const navRef = useRef<HTMLDivElement>(null);
 
@@ -26,22 +22,11 @@ function Navbar() {
         ref={navRef}
       >
         <div className={`${flexCenter} gap-x-2`}>
-          <GiHamburgerMenu size={25} onClick={() => cycleOpen()} />
+          <GiHamburgerMenu size={25} onClick={() => {}} />
           <p className='text-sm font-semibold text-grey'>현진민 학원</p>
         </div>
         <p className='text-xs text-grey'>로그아웃</p>
       </nav>
-      <AnimatePresence>
-        {open && (
-          <div className='fixed inset-0 z-50'>
-            <div
-              className='absolute inset-0 z-50 bg-black opacity-50'
-              onClick={() => cycleOpen()}
-            />
-            <SideBar />
-          </div>
-        )}
-      </AnimatePresence>
 
       <div
         className='flex-grow w-full overflow-auto'
